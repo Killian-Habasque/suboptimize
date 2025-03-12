c4a8742fddccb42e7518a07c46be73a1ebe5d3ca
Killian Habasque <killian.habasque@gmail.com>
2025-03-12 21:53:01 +0100

    refacto & type calendar component

--- src/pages/subscriptions/index.tsx
+++ src/pages/subscriptions/index.tsx
@@ -1,40 +1,10 @@
-// import { useAuth } from "../../contexts/authContext";
 import { useSubscription } from "../../contexts/subscriptionContext";
-// import { addSubscription } from "../../services/subscriptionService";
-import Calendar from '../../components/calendar_sub.jsx'
+import Calendar from '../../components/calendar_sub';
 
 const Subscriptions = () => {
-    // const { currentUser } = useAuth();
     const { subscriptions, loading } = useSubscription();
-
-    // const handleAddSubscription = async () => {
-    //     if (!currentUser) {
-    //         alert("Vous devez être connecté pour ajouter un abonnement.");
-    //         return;
-    //     }
-
-    //     try {
-    //         await addSubscription(
-    //             "Abonnement Exemple New",
-    //             new Date("2025-01-01"),
-    //             new Date("2025-12-31"),
-    //             15
-    //         );
-    //         alert("Abonnement ajouté avec succès !");
-    //     } catch (error) {
-    //         console.error("Erreur lors de l'ajout de l'abonnement :", error);
-    //         alert("Impossible d'ajouter l'abonnement. Veuillez réessayer.");
-    //     }
-    // };
-
     return (
         <div className="text-2xl font-bold pt-14">
-            {/* <button
-                className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
-                onClick={handleAddSubscription}
-            >
-                Ajouter un abonnement
-            </button> */}
             {loading ? (
                 <p>Chargement des abonnements...</p>
             ) : subscriptions.length > 0 ? (
--- src/services/utils.ts
+++ src/services/utils.ts
@@ -2,3 +2,8 @@
 export function classNames(...classes: string[]) {
     return classes.filter(Boolean).join(' ')
 }
+
+export function capitalizeFirstLetter(string: string) {
+    if (!string) return '';
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
